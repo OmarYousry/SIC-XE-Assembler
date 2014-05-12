ad30ea7587e0eaf2a908927f88f6d0efd36b7eea
omaryousry <omaryousry@omaryousry-HP>
2014-05-12 13:26:59 +0200

    Hello World

---
+++ SIC_XE Assembler/src/HelloWorld.cpp
@@ -0,0 +1,17 @@
+/*
+ * HelloWorld.cpp
+ *
+ *  Created on: May 12, 2014
+ *      Author: omaryousry
+ */
+# include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+	return 0;
+}
+
+
+
